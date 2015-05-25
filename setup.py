9bd2a2127e5a5b25aa8db44f07e9000010ad916a
Alexander Stefanov <alexander.stefanov@lulin.bg>
2015-05-25 10:11:07 +0300

    .

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='ZhongGuoLib',
+    version='0.0.2',
+    author='Alexander Stefanov',
+    author_email='alexander.stefanov@lulin.bg',
+    packages=['zhongguolib', 'zhongguolib.test'],
+    url='https://github.com/lalstef/zhongguolib',
+    license='LICENSE.txt',
+    description='Useful chinese language stuff.',
+    long_description=open('README.txt').read(),
+)
